2044b89077c802067442b88f33609a3064b02b90
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-10-06 13:28:53 +0300

    фикс

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-const getRandomInt = function(min, max) {
+function getRandomInt(min, max) {
     if (min >= 0 && max > min) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    return 'Ввели неверные значения';
-};
-
-getRandomInt(0, 0.1);
-
+    return 'Ввели не верный значения';
+}
 const getRandomCoordinate = function(min, max, decimals) {
     if (min >= 0 && max > min) {
         let coordinates = Math.random() * (max - min + 1) + min;
         return coordinates.toFixed(decimals);
     }
     return 'Ввели не верный значения';
-}
-
+};
 getRandomCoordinate(0, 10, 1)
+getRandomInt(0, 0.1);
