19e5c0bc827e31858ffb09f65508ca0651ebb7f3
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-10-06 14:40:40 +0300

    привел к правильному виду

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   if (min >= 0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return 'Ввели не верный значения';
-}
-const getRandomCoordinate = function(min, max, decimals) {
+};
+const getRandomCoordinate = (min, max, decimals) => {
   if (min >= 0 && max > min) {
     const num = Math.random() * (max - min + 1) + min;
     return num.toFixed(decimals);
