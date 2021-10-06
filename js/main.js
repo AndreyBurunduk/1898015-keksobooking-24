2908c6a73f1b521eb8c6e0bf6567ada8665d1f96
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-10-06 13:49:14 +0300

    убрл лишние пробелы

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 function getRandomInt(min, max) {
-    if (min >= 0 && max > min) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return 'Ввели не верный значения';
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return 'Ввели не верный значения';
 }
 const getRandomCoordinate = function(min, max, decimals) {
-    if (min >= 0 && max > min) {
-        let num = Math.random() * (max - min + 1) + min;
-        return num.toFixed(decimals);
-    }
-    return 'Ввели не верный значения';
+  if (min >= 0 && max > min) {
+    const num = Math.random() * (max - min + 1) + min;
+    return num.toFixed(decimals);
+  }
+  return 'Ввели не верный значения';
 };
-getRandomCoordinate(0, 10, 1)
+getRandomCoordinate(0, 10, 1);
 getRandomInt(0, 0.1);
