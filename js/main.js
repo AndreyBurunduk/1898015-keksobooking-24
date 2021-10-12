4db18617aa590be031e54f231e123b6348ab2624
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-10-12 12:22:10 +0300

    поменял ковычки на одинарные

--- js/main.js
+++ js/main.js
@@ -18,21 +18,21 @@ const getRandomCoordinate = (min, max, decimals) => {
 const getAvatar = () => {
   const number = getRandomInt(1, 10);
   if (number >= 10) {
-    return "img/avatars/user" + "0" + number + ".png";
+    return 'img/avatars/user' + '0' + number + '.png';
   }
-  return "img/avatars/user" + number + ".png";
+  return 'img/avatars/user' + number + '.png';
 }; //avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
 
 const TITLE = [
-  "Световая симфония",
-  "Карнавал текстур и красок",
-  "Девичье гнездышко",
-  "Приют всех муз",
-  "Яркое отражение индивидуальности",
-  "Романтика мегаполиса",
-  "Дыхание природы",
-  "Изящество классики, уют прованса",
-  "Надежный приют",
+  'Световая симфония',
+  'Карнавал текстур и красок',
+  'Девичье гнездышко',
+  'Приют всех муз',
+  'Яркое отражение индивидуальности',
+  'Романтика мегаполиса',
+  'Дыхание природы',
+  'Изящество классики, уют прованса',
+  'Надежный приют',
 ];//title, строка — заголовок предложения. Придумайте самостоятельно.
 
 const getAddress = () =>{
@@ -42,42 +42,42 @@ const getAddress = () =>{
 }; //address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
 
 const TYPE = [
-    "palace",
-    "flat",
-    "house",
-    "bungalow",
-    "hotel",
+    'palace',
+    'flat',
+    'house',
+    'bungalow',
+    'hotel',
 ]; //type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
 
 const CHECKIN = [
-    "12:00",
-    "13:00",
-    "14:00",
+    '12:00',
+    '13:00',
+    '14:00',
 ]; //checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
 
 const CHECKOUT = [
-    "12:00",
-    "13:00",
-    "14:00",
+    '12:00',
+    '13:00',
+    '14:00',
 ]; //checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
 
 const FEATURES = [
-    "wifi",
-    "dishwasher",
-    "parking",
-    "washer",
-    "elevator",
-    "conditioner",
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
 ]; //features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
 
 const DESCRIPTION = [
-    "В художественном произведении интерьер не просто показывает условия жизни персонажей..."
+    'В художественном произведении интерьер не просто показывает условия жизни персонажей...',
 ]; //description, строка — описание помещения. Придумайте самостоятельно.
 
 const PHOTOS = [
-    "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
-    "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
-    "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg."
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.',
 ]; //photos, массив строк — массив случайной длины из значений:
 
 const getRandomElement = (elements) => {
