f6a2e4dc9afd7bcc2fe6a6d8d33c28fafdbadecf
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-10-13 09:43:52 +0300

    все ковычки заменил на обратные

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const getRandomInt = (min, max) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return 'Ввели не верный значения';
+  return `Ввели не верный значения`;
 };
 
 const getRandomCoordinate = (min, max, decimals) => {
@@ -12,45 +12,45 @@ const getRandomCoordinate = (min, max, decimals) => {
     const num = Math.random() * (max - min + 1) + min;
     return num.toFixed(decimals);
   }
-  return 'Ввели не верный значения';
+  return `Ввели не верный значения`;
 };
 
 const getAvatar = () => {
-  const randomNumber = '0${getRandomInt(1, 10)}'.slice(-2);
-  return 'img/avatars/user${randomNumber}.png';
+  const randomNumber = `0${getRandomInt(1, 10)}`.slice(-2);
+  return `img/avatars/user${randomNumber}.png`;
 }; //avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
 
 const TITLE = [
-  'Световая симфония',
-  'Карнавал текстур и красок',
-  'Девичье гнездышко',
-  'Приют всех муз',
-  'Яркое отражение индивидуальности',
-  'Романтика мегаполиса',
-  'Дыхание природы',
-  'Изящество классики, уют прованса',
-  'Надежный приют',
+  `Световая симфония`,
+  `Карнавал текстур и красок`,
+  `Девичье гнездышко`,
+  `Приют всех муз`,
+  `Яркое отражение индивидуальности`,
+  `Романтика мегаполиса`,
+  `Дыхание природы`,
+  `Изящество классики, уют прованса`,
+  `Надежный приют`,
 ]; //title, строка — заголовок предложения. Придумайте самостоятельно.
 
-const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel']; //type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
+const TYPE = [`palace`, `flat`, `house`, `bungalow`, `hotel`]; //type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
 
-const CHECKIN = ['12:00', '13:00', '14:00']; //checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+const CHECKIN = [`12:00`, `13:00`, `14:00`]; //checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
 
-const CHECKOUT = ['12:00', '13:00', '14:00']; //checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
+const CHECKOUT = [`12:00`, `13:00`, `14:00`]; //checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
 
 const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
+  `wifi`,
+  `dishwasher`,
+  `parking`,
+  `washer`,
+  `elevator`,
+  `conditioner`,
 ]; //features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
 
 const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.',
+  `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg`,
+  `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg`,
+  `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.`,
 ]; //photos, массив строк — массив случайной длины из значений:
 
 const getRandomElement = (elements) => {
@@ -71,7 +71,7 @@ const createWizard = () => {
     offer: {
       title: getRandomElement(TITLE),
       price: getRandomInt(1000, 10000),
-      address: '${location.lat}, ${location.lng}',
+      address: `${location.lat}, ${location.lng}`,
       type: getRandomElement(TYPE),
       rooms: getRandomInt(1, 5),
       quests: getRandomInt(1, 5),
@@ -79,7 +79,7 @@ const createWizard = () => {
       checkout: getRandomElement(CHECKOUT),
       features: getRandomElement(FEATURES),
       description:
-        'В художественном произведении интерьер не просто показывает условия жизни персонажей...',
+        `В художественном произведении интерьер не просто показывает условия жизни персонажей...`,
       photos: getRandomElement(PHOTOS),
       location,
     },
