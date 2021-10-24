7de9569c5013aaf47e11d7ec1217ff062d9a7d80
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-10-24 19:54:55 +0300

    исправление

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import { generatingObject } from './data.js';
-
-
-
 //поиск места вставки объявления и поиск шаблонна для вставки
 const placeToInsert = document.querySelector('.map__canvas');
 const cardPattern = document.querySelector('#card').content.querySelector('.popup');
