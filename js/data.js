20b1abf9f927fc486c22a974ef1e887d2312013b
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-11-02 15:42:18 +0300

    поправил вывод координат

--- js/data.js
+++ js/data.js
@@ -2,14 +2,18 @@ import { getRandomInt, getRandomCoordinate, getRandomElement, randomNumber, getA
 import { TITLES, TYPES, HOURS, FEATURES, PHOTOS, DESCRIPTION } from './loading.js';
 
 //создание объекта
-const createObject = () => ({
-  author: {
+const createObject = () => {
+  const author = new Object({
     avatar: `img/avatars/user${randomNumber()}.png`,
-  },
-  offer: {
+  });
+  const location = new Object({
+    lat: `${getRandomCoordinate(35.65, 35.7, 5)}`,
+    lng: `${getRandomCoordinate(139.7, 139.8, 5)}`,
+  });
+  const offer = new Object({
     title: getRandomElement(TITLES),
     price: getRandomInt(1000, 10000),
-    address: `${location.lat}, ${location.lng}`,
+    address: `${location.lat} , ${location.lng}`,
     type: getRandomElement(TYPES),
     rooms: getRandomInt(1, 5),
     quests: getRandomInt(1, 5),
@@ -18,12 +22,13 @@ const createObject = () => ({
     features: getArray(FEATURES),
     description: getRandomElement(DESCRIPTION),
     photos: getArray(PHOTOS),
-  },
-  location: {
-    lat: getRandomCoordinate(35.65, 35.7, 5),
-    lng: getRandomCoordinate(139.7, 139.8, 5),
-  },
-});
+  });
+  return {
+    author: author,
+    offer: offer,
+    location: location,
+  };
+};
 
 export const getArrElement = (COUNT_ELEMENT = 1) =>
   Array.from({ length: COUNT_ELEMENT }, createObject);
