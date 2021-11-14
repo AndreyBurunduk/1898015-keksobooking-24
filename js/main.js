2946ceda6c1b245964a75b5158b20c32309af224
Andrey Kosov < emailAndrejcosov@yandex.ru>
2021-11-14 17:59:32 +0300

    правка

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 import { validateForm } from './form.js';
 import { initMap } from './map.js';
 import { onFormSubmit } from './msg.js';
-//import { setPreviews } from './previews.js';
+
 import { makeInactive } from './activation.js';
 
 makeInactive ();
 initMap();
 validateForm();
-//setPreviews();
+
 onFormSubmit();
